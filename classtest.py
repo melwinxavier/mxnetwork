0c64ae537bc2592287fc865aa8bab45df0b0af9e
melwinxavier <44630419+melwinxavier@users.noreply.github.com>
2018-11-01 13:45:42 +0530

    Add files via upload

---
+++ classtest.py
@@ -0,0 +1,10 @@
+number = input("enter the values")
+class Nub:
+	def __init__(self,number):
+		self.number=number
+	def printNumber(self):
+		print("number are %s" %(self.number))
+s=Nub(number)
+s.printNumber()
+
+
---
+++ sort.py
@@ -0,0 +1,4 @@
+a = [1,2,3,4,4,5,5,5]
+sorted(a,key=a.count, reverse=True)
+print(a.sort)
+
